2a5ef4141845e2d0f3a6300334a71031725285ce
esmartdie <esmartdie@gmail.com>
2024-04-14 21:04:11 +0200

    initial commit

---
+++ src/main/java/com/esmartdie/EsmartCafeteriaApi/EsmartCafeteriaApiApplication.java
@@ -0,0 +1,13 @@
+package com.esmartdie.EsmartCafeteriaApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsmartCafeteriaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsmartCafeteriaApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/esmartdie/EsmartCafeteriaApi/model/User.java
@@ -0,0 +1,9 @@
+package com.esmartdie.EsmartCafeteriaApi.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class User {
+
+
+}
