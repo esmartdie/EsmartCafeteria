ba19eb8608a8623624aa9524fa2688ada10794fa
esmartdie <esmartdie@gmail.com>
2024-05-11 16:49:03 +0200

    moving to new sub package

--- src/main/java/com/esmartdie/EsmartCafeteriaApi/security/SecurityConfig.java
+++ src/main/java/com/esmartdie/EsmartCafeteriaApi/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.esmartdie.EsmartCafeteriaApi.security;
 
+import com.esmartdie.EsmartCafeteriaApi.security.filters.CustomAuthenticationFilter;
+import com.esmartdie.EsmartCafeteriaApi.security.filters.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -14,11 +16,6 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import static org.springframework.http.HttpMethod.*;
-
-import static org.springframework.http.HttpMethod.GET;
-import static org.springframework.http.HttpMethod.POST;
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 
 @Configuration
